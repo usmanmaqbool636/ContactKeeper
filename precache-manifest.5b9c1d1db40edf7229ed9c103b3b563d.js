4cc64590d5e7052f99ec9a957baf6f2d20f97682
usman maqbool <usmanmaqbool636@gmail.com>
2020-05-15 02:23:11 +0500

    Updates

---
+++ precache-manifest.5b9c1d1db40edf7229ed9c103b3b563d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09191dfa0a32b88ea2e709b4114240fa",
+    "url": "/ContactKeeper.github.io/index.html"
+  },
+  {
+    "revision": "72b5b257011d8af979f7",
+    "url": "/ContactKeeper.github.io/static/css/main.5812e380.chunk.css"
+  },
+  {
+    "revision": "a818af4cd2271c97711a",
+    "url": "/ContactKeeper.github.io/static/js/2.288cb857.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ContactKeeper.github.io/static/js/2.288cb857.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72b5b257011d8af979f7",
+    "url": "/ContactKeeper.github.io/static/js/main.5248e215.chunk.js"
+  },
+  {
+    "revision": "0684c4aa13d2857c5d95",
+    "url": "/ContactKeeper.github.io/static/js/runtime-main.27775be4.js"
+  }
+]);
