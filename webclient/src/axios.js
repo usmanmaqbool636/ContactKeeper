bf90422258829549ca19ed6dfc34d7dfc211ba86
usman maqbool <usmanmaqbool636@gmail.com>
2020-05-16 19:53:05 +0500

    change backend url

--- webclient/src/axios.js
+++ webclient/src/axios.js
@@ -1,6 +1,14 @@
 import axios from 'axios';
-const instance = axios.create({
-    baseURL: 'http://localhost:5000',
-});
+let instance;
+if (process.env.NODE_ENV !== "production") {
 
+    instance = axios.create({
+        baseURL: 'http://localhost:5000',
+    });
+}
+else {
+    instance = axios.create({
+        baseURL: 'http://localhost:5000',
+    });
+}
 export default instance;
